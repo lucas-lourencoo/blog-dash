543ff9933f6e91bbd31fa23642a4a6828645f761
Lucas Lourenço <lucas.silva@dotkon.co>
2023-05-16 14:00:44 -0400

    fix: routes and images

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -8,6 +8,10 @@ export const Container = styled("main", {
   height: "100vh",
   gridTemplateColumns: "25fr 75fr",
 
+  ".archivesList": {
+    listStyle: "none",
+  },
+
   form: {
     marginTop: "5rem",
   },
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://blog-dash.vercel.app/api",
 });
